bd5d0153a3bf4be50c4ca5b0425b06eccc327e81
jordi <jordimartinezcarbajal14@gmail.com>
2022-07-17 18:32:12 -0600

    Codificacion Clientes y modificacion bd

--- FarmaciaBotiquin/Clases/Clientes.cs
+++ FarmaciaBotiquin/Clases/Clientes.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace FarmaciaBotiquin.Clases
 {
@@ -17,5 +20,115 @@ namespace FarmaciaBotiquin.Clases
         public int IdEstado { get; set; }
 
         Conexion conexion = new Conexion();
+
+        public void MostrarCliente(DataGridView dataGrid)
+        {
+            try
+            {
+                conexion.sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("Sp_Cliente", conexion.sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                sqlCommand.Parameters.AddWithValue("@accion", "mostrar");
+                using (sqlDataAdapter)
+                {
+                    DataTable dataTable = new DataTable();
+                    sqlDataAdapter.Fill(dataTable);
+                    dataGrid.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conexion.sqlConnection.Close();
+            }
+        }
+        public void BuscarCliente(DataGridView dataGrid, string valorBuscado)
+        {
+
+            try
+            {
+                conexion.sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("Sp_Cliente", conexion.sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                sqlCommand.Parameters.AddWithValue("@BuscarCliente", valorBuscado);
+                sqlCommand.Parameters.AddWithValue("@accion", "buscar");
+
+                using (sqlDataAdapter)
+                {
+                    DataTable dataTable = new DataTable();
+                    sqlDataAdapter.Fill(dataTable);
+                    dataGrid.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conexion.sqlConnection.Close();
+            }
+        }
+        
+ 
+        public void AgregarCliente(Clientes cliente)
+        {
+            try
+            {
+                conexion.sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand("Sp_Cliente", conexion.sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@DNICliente", cliente.DNICliente);
+                sqlCommand.Parameters.AddWithValue("@NombreCliente", cliente.NombreCliente);
+                sqlCommand.Parameters.AddWithValue("@ApellidoCliente", cliente.ApellidoCliente);
+                sqlCommand.Parameters.AddWithValue("@Email", cliente.Email);
+                sqlCommand.Parameters.AddWithValue("@FechaNacimiento", cliente.FechaNacimiento);
+                sqlCommand.Parameters.AddWithValue("@IdEstado", cliente.IdEstado);
+                sqlCommand.Parameters.AddWithValue("@accion", "insertar");
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conexion.sqlConnection.Close();
+            }
+        }
+        public void EditarCliente(Clientes cliente)
+        {
+            try
+            {
+                conexion.sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("Sp_Cliente", conexion.sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                sqlCommand.Parameters.AddWithValue("@IdCliente", cliente.IdCliente);
+                sqlCommand.Parameters.AddWithValue("@DNICliente", cliente.DNICliente);
+                sqlCommand.Parameters.AddWithValue("@NombreCliente", cliente.NombreCliente);
+                sqlCommand.Parameters.AddWithValue("@ApellidoCliente", cliente.ApellidoCliente);
+                sqlCommand.Parameters.AddWithValue("@Email", cliente.Email);
+                sqlCommand.Parameters.AddWithValue("@FechaNacimiento", cliente.FechaNacimiento);
+                sqlCommand.Parameters.AddWithValue("@IdEstado", cliente.IdEstado);
+
+                sqlCommand.Parameters.AddWithValue("@accion", "editar");
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conexion.sqlConnection.Close();
+            }
+        }
     }
 }
--- FarmaciaBotiquin/FrmCliente.cs
+++ FarmaciaBotiquin/FrmCliente.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 using FarmaciaBotiquin.Clases;
 
@@ -14,7 +15,6 @@ namespace FarmaciaBotiquin
     public partial class FrmCliente : Form
     {
         Clientes cliente = new Clientes();
-        Puesto puesto = new Puesto();
         Estado estado = new Estado();
         Validaciones validaciones = new Validaciones();
 
@@ -24,6 +24,14 @@ namespace FarmaciaBotiquin
         {
             InitializeComponent();
             estado.CargarComboboxEstado(cmbEstado);
+            inicializarDatagrid();
+        }
+
+        private void inicializarDatagrid()
+        {
+            cliente.MostrarCliente(dgvClientes);
+            dgvClientes.Columns["IdCliente"].Visible = false;
+
         }
 
         private void pbCerrar_Click(object sender, EventArgs e)
@@ -31,6 +39,62 @@ namespace FarmaciaBotiquin
             Form formulario = new FrmPrincipal();
             this.Close();
             this.Show();
+        }
+
+        private bool VerificarParametros()
+        {
+            if (!validaciones.verificarTextoLargo(txtDniCliente.Text, 13))
+            {
+                MessageBox.Show("Por favor, ingrese el DNI del cliente");
+                return false;
+            }
+            if (!validaciones.verificarTextoLargo(txtNombreCliente.Text, 30))
+            {
+                MessageBox.Show("Por favor, ingrese el nombre del cliente.");
+                return false;
+            }
+
+            if (!validaciones.verificarTextoLargo(txtApellidosCliente.Text, 30))
+            {
+                MessageBox.Show("Por favor, ingrese el apellido del cliente");
+                return false;
+            }
+
+            if (!validaciones.verificarTextoLargo(txtCorreoClientes.Text, 25))
+            {
+                MessageBox.Show("Ingrese un correo para el cliente");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ObtenerParametros()
+        {
+            cliente.DNICliente = txtDniCliente.Text;
+            cliente.NombreCliente = txtNombreCliente.Text;
+            cliente.ApellidoCliente = txtApellidosCliente.Text;
+            cliente.Email = txtCorreoClientes.Text;
+            cliente.FechaNacimiento = dtpFechaNacClientes.Value;
+            cliente.IdEstado = (int)cmbEstado.SelectedValue;
+        }
+        private void limpieza()
+        {
+            foreach (Control ctr in gbEmpleado.Controls)
+            {
+                if (ctr is TextBox)
+                    ctr.Text = "";
+            }
+            inicializarDatagrid();
+            cmbEstado.SelectedValue = 1;
+            telefono = "";
+            correo = "";
+        }
+
+        private void limpiezaMax()
+        {
+            limpieza();
+            seleccionActiva = false;
         }
 
         private void pnRight_Paint(object sender, PaintEventArgs e)
@@ -45,7 +109,7 @@ namespace FarmaciaBotiquin
 
         private void txtBuscar_TextChanged(object sender, EventArgs e)
         {
-
+            cliente.BuscarCliente(dgvClientes, txtBuscar.Text);
         }
 
         private void dgvClientes_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -55,12 +119,30 @@ namespace FarmaciaBotiquin
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-
+            if (VerificarParametros())
+            {
+                ObtenerParametros();
+                cliente.AgregarCliente(cliente);
+                limpieza();
+            }
         }
 
         private void btnEditar_Click(object sender, EventArgs e)
         {
+            if (!seleccionActiva)
+            {
+                MessageBox.Show("Por favor, selecione un cliente para editarlo");
+            }
+            else
+            {
+                if (VerificarParametros())
+                {
 
+                    ObtenerParametros();
+                    cliente.EditarCliente(cliente);
+                    limpiezaMax();
+                }
+            }
         }
 
         private void lbEstado_Click(object sender, EventArgs e)
@@ -131,6 +213,40 @@ namespace FarmaciaBotiquin
         private void txtDniCliente_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void txtDniCliente_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Validaciones.SoloNumeros(e);
+        }
+
+        private void txtNombreCliente_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Validaciones.SoloLetras(e);
+        }
+
+        private void txtApellidosCliente_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Validaciones.SoloLetras(e);
+        }
+
+        private void dgvClientes_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvClientes.Rows[e.RowIndex];
+                cliente.IdCliente = int.Parse(row.Cells["IdCliente"].Value.ToString());
+
+                txtDniCliente.Text = row.Cells["DNICliente"].Value.ToString();
+                txtNombreCliente.Text = row.Cells["Nombre"].Value.ToString();
+                txtApellidosCliente.Text = row.Cells["Apellidos"].Value.ToString();
+                txtCorreoClientes.Text = row.Cells["Email"].Value.ToString();
+                dtpFechaNacClientes.Value = Convert.ToDateTime(dgvClientes.Rows[e.RowIndex].Cells["FechaNacimiento"].Value.ToString());
+                cmbEstado.Text = row.Cells["Estado"].Value.ToString();
+
+                correo = row.Cells["Email"].Value.ToString();
+                seleccionActiva = true;
+            }
         }
 
         private void cmbEstado_SelectedIndexChanged(object sender, EventArgs e)
